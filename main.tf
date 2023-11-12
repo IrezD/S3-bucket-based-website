292ef28d98af1b321704e5e4bd352c9bdca4871b
IrezD <dennysben@gmail.com>
2023-11-12 17:38:10 +0100

    Deploying S3 bucket and tfstate file

--- main.tf
+++ main.tf
@@ -8,52 +8,52 @@ resource "aws_s3_bucket" "Tfstate_bucket" {
   bucket = "s3prod-tfstatefile001"
 }
 
-# resource "aws_s3_bucket_versioning" "Tfstate_versioning" {
-#   bucket = aws_s3_bucket.Tfstate_bucket.bucket
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "Tfstate_versioning" {
+  bucket = aws_s3_bucket.Tfstate_bucket.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# terraform {
-#   backend "s3" {
-#     bucket = "s3prod-tfstatefile001"
-#     key = "S3_terraform.tfstate"
-#     region = "eu-central-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "s3prod-tfstatefile001"
+    key = "S3_terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
 
-# # ********* S3 website content bucket ********
+# ********* S3 website content bucket ********
 
-# resource "aws_s3_bucket" "S3Prod_bucket" {
-#   bucket = "S3Prod-static-website001"
-#   tags = {
-#     Name = "S3Prod tf-static website"
-#   }
-# }
+resource "aws_s3_bucket" "S3Prod_bucket" {
+  bucket = "s3prod-static-website001"
+  tags = {
+    Name = "S3Prod tf-static website"
+  }
+}
 
-# resource "aws_s3_bucket_public_access_block" "S3Prod_publicPolicy" {
-#     bucket = aws_s3_bucket.S3Prod_bucket.bucket
+resource "aws_s3_bucket_public_access_block" "S3Prod_publicPolicy" {
+    bucket = aws_s3_bucket.S3Prod_bucket.bucket
     
-#     block_public_acls       = false
-#     block_public_policy     = false
-#     ignore_public_acls      = false
-#     restrict_public_buckets = false
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
 
-# }
+}
 
-# resource "aws_s3_bucket_website_configuration" "S3Prod_index" {
-#   bucket = aws_s3_bucket.S3Prod_bucket.id
+resource "aws_s3_bucket_website_configuration" "S3Prod_index" {
+  bucket = aws_s3_bucket.S3Prod_bucket.id
 
-#   index_document {
-#     suffix = "index.html"
-#   }
-# }
+  index_document {
+    suffix = "index.html"
+  }
+}
 
-# resource "aws_s3_object" "S3Prod_content" {
-#   bucket = aws_s3_bucket.S3Prod_bucket.bucket
-#   key    = "index.html"
-#   source = "index.html"
-#   etag = filemd5("index.html")
-# }
+resource "aws_s3_object" "S3Prod_content" {
+  bucket = aws_s3_bucket.S3Prod_bucket.bucket
+  key    = "index.html"
+  source = "index.html"
+  etag = filemd5("index.html")
+}
 
