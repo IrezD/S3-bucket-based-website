85aa6d1ccb3bc288937c2f3726c05c0d07683747
IrezD <dennysben@gmail.com>
2023-11-12 09:25:37 +0100

    Deploying VPC and Subnets

--- main.tf
+++ main.tf
@@ -9,3 +9,19 @@ terraform {
     region = "eu-central-1"
   }
 }
+
+resource "aws_vpc" "S3Prod" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "S3 Website Host"
+  }
+}
+
+resource "aws_subnet" "S3Prod" {
+    vpc_id = aws_vpc.S3Prod.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "S3 Website Subnet"
+    }
+}
+
