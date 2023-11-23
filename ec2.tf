48cbe38fa2c5f5f9e184902e473a2cf9e48fff6d
IrezD <dennysben@gmail.com>
2023-11-23 23:40:10 +0100

    Deleted ec2 infra and file

--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "testInstance" {
-  ami = "ami-0669b163befffbdfc"
-  instance_type = "t2.micro"
-  availability_zone = "eu-central-1c"
-  tags = {
-    Name = "DemoInstance Deployment"
-  }
-}
