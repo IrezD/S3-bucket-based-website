a58c4da6cf36043142ea7e3eb7463c6018113cd9
IrezD <dennysben@gmail.com>
2023-11-24 02:08:07 +0100

    Only s3

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,66 +1,66 @@
-# ********* CloudFront Distribution with S3 bucket as origin ********
+# # ********* CloudFront Distribution with S3 bucket as origin ********
 
-locals {
-  s3_origin_id = "S3Prod_Origin_ID"
-}
+# locals {
+#   s3_origin_id = "S3Prod_Origin_ID"
+# }
 
-resource "aws_cloudfront_origin_access_control" "S3Prod_cdn_origin-access-control" {
-    name                              = "S3Prod_Orgin access control"
-    description                       = "This is an origin access control for my S3 static website"
-    origin_access_control_origin_type = "s3"
-    signing_behavior                  = "always"
-    signing_protocol                  = "sigv4"
-}
+# resource "aws_cloudfront_origin_access_control" "S3Prod_cdn_origin-access-control" {
+#     name                              = "S3Prod_Orgin access control"
+#     description                       = "This is an origin access control for my S3 static website"
+#     origin_access_control_origin_type = "s3"
+#     signing_behavior                  = "always"
+#     signing_protocol                  = "sigv4"
+# }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket.S3Prod_bucket.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.S3Prod_cdn_origin-access-control.id
-    origin_id                = local.s3_origin_id
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Comment about deploying CDN for my S3 bucket"
-  default_root_object = "index.html"
-
-#   logging_config {
-#     include_cookies = false
-#     bucket          = aws_s3_bucket.s3prod_log.bucket
-#     prefix          = "myprefix"
+# resource "aws_cloudfront_distribution" "s3_distribution" {
+#   origin {
+#     domain_name              = aws_s3_bucket.S3Prod_bucket.bucket_regional_domain_name
+#     origin_access_control_id = aws_cloudfront_origin_access_control.S3Prod_cdn_origin-access-control.id
+#     origin_id                = local.s3_origin_id
 #   }
 
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   comment             = "Comment about deploying CDN for my S3 bucket"
+#   default_root_object = "index.html"
 
-    forwarded_values {
-      query_string = false
+# #   logging_config {
+# #     include_cookies = false
+# #     bucket          = aws_s3_bucket.s3prod_log.bucket
+# #     prefix          = "myprefix"
+# #   }
 
-      cookies {
-        forward = "none"
-      }
-    }
+#   default_cache_behavior {
+#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = local.s3_origin_id
 
-    viewer_protocol_policy = "allow-all" # --- I will change this to 'redirect-to-https' after initial deployment
-    min_ttl                = 0
-    default_ttl            = 3600        # --- I'll play around with the TTL after initial deployment
-    max_ttl                = 86400
-  }
+#     forwarded_values {
+#       query_string = false
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
-    }
-  }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-  tags = {
-    Name = "S3Prod_content distribution"
-  }
+#     viewer_protocol_policy = "allow-all" # --- I will change this to 'redirect-to-https' after initial deployment
+#     min_ttl                = 0
+#     default_ttl            = 3600        # --- I'll play around with the TTL after initial deployment
+#     max_ttl                = 86400
+#   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}  
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "whitelist"
+#       locations        = ["US", "CA", "GB", "DE"]
+#     }
+#   }
+
+#   tags = {
+#     Name = "S3Prod_content distribution"
+#   }
+
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
+# }  
