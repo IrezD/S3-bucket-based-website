36c39264d9a650b87b3df30e2454eac02e19b9c3
IrezD <dennysben@gmail.com>
2023-11-29 09:52:07 +0100

    appropriate CF distribution attribute passed in custom domain configuration

--- route53.tf
+++ route53.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_record" "s3Prod_cdn_config" {
-  zone_id = "Z0822301268JPSC1M6XE6"
+  zone_id = "Z0822301268JPSC1M6XE6" #-- Will be improved on to have data source dynamically retrieve the ID instead of hardcoding
   name    = "dennisowie.com"
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.dns_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = true
   }
 }
