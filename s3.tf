754f197ae891b26f74d235cf2497d9f6c6959d58
IrezD <dennysben@gmail.com>
2023-11-30 21:52:12 +0100

    Bucket Policy updated to allow dev-distribution fetch objects

--- s3.tf
+++ s3.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "s3Prod_policy" {
     condition {
       test = "StringEquals"
       variable = "AWS:SourceArn"
-      values = [aws_cloudfront_distribution.s3_distribution.arn]
+      values = [aws_cloudfront_distribution.s3_distribution.arn, aws_cloudfront_distribution.s3_dev-distribution.arn]
     }
   }
 }
